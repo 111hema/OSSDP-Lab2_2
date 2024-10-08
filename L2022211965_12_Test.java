225003647d4c0f435e84b8d438c36c57d4912c8f
Hema <hhippopotamus@163.com>
2024-10-08 13:57:30 +0800

    fixed

---
+++ L2022211965_12_Test.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution12Test {
+
+    /**
+     * Test two small numbers.
+     */
+    @Test
+    void testSmallNumbers() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("6", multiplier.multiply("2", "3"));
+    }
+
+    /**
+     * Test two big numbers.
+     */
+    @Test
+    void testBigNumbers() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("56088", multiplier.multiply("123", "456"));
+    }
+
+    /**
+     * Test one 0.
+     */
+    @Test
+    void testZeroMultiplier() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("0", multiplier.multiply("0", "9999"));
+    }
+
+    /**
+     * Test two 0.
+     */
+    @Test
+    void testBothZeroMultiplier() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("0", multiplier.multiply("0", "0"));
+    }
+
+    /**
+     * Test different length numbers.
+     */
+    @Test
+    void testDifferentLengthMultiplier() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("8369910", multiplier.multiply("12345", "678"));
+    }
+
+    /**
+     * Test max boundary.
+     */
+    @Test
+    void testMaxBoundary() {
+        Solution12 multiplier = new Solution12();
+        assertEquals("999999998000000001", multiplier.multiply("999999999", "999999999"));
+    }
+}
